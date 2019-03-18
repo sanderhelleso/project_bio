4ed67b74a7c4ae262aaab5ce51d96fb65d89b899
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-18 13:50:04 -0700

    added .env

--- server/main.go
+++ server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("HELLO WORLD")
+
+	// ! REMEMBER TO ADD TO .gitigore IF EVER GOING PUBLIC REPO !
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:5000
 }
