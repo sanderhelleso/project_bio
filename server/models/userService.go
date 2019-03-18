5b3fcae855fe71c1a49cdc369e22e306b76e3bc5
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-18 14:47:30 -0700

    Added core DB utils

--- server/models/userService.go
+++ server/models/userService.go
@@ -21,8 +21,9 @@ var (
 
 // User represent a user in the project
 type User struct {
-	Username     string `json:"username"`
-	InstagramURL string `json:"instagramURL"`
+	gorm.Model          // `ID`, `CreatedAt`, `UpdatedAt`, `DeletedAt`
+	Username     string `gorm:"size:30;not null;unique_index"`
+	InstagramURL string `gorm:"unique"`
 }
 
 // UserService represents a connection layer
@@ -39,7 +40,7 @@ type UserService struct {
 // - If the connection was not successfull, return nil for 'UserService'
 //	and the error that occured during initialization of method
 func NewUserService(connectionInfo string) (*UserService, error) {
-	db, err := gorm.Open(os.Getenv("database"), connectionInfo)
+	db, err := gorm.Open(os.Getenv("db"), connectionInfo)
 	if err != nil {
 		return nil, err
 	}
@@ -47,4 +48,24 @@ func NewUserService(connectionInfo string) (*UserService, error) {
 	return &UserService{
 		db,
 	}, nil
+}
+
+// AutoMigrate will attempt to automatically migrate the users table
+func (us *UserService) AutoMigrate() error {
+	err := us.db.AutoMigrate(&User{}).Error
+	return err
+}
+
+// DestructiveReset drops the existing user table and rebuilds it
+func (us *UserService) DestructiveReset() error {
+	if err := us.db.DropTableIfExists(&User{}).Error; err != nil {
+		return err
+	}
+
+	return us.AutoMigrate()
+}
+
+// Close will close the user service connection to the database
+func (us *UserService) Close() error {
+	return us.db.Close()
 }
