22bfb6fab8ec5df5b50a724bb7f90f2162333d46
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-24 20:01:05 -0700

    Added promo reducer

--- client/src/actions/actionTypes.js
+++ client/src/actions/actionTypes.js
@@ -1,15 +1,22 @@
+// USER REDUCER
 export const LOGIN           = 'LOGIN';
 export const LOGOUT          = 'LOGOUT';
 export const DELETE_ACCOUNT  = 'DELETE_ACCOUNT';
+
+// FOLLOWER REDUCER
+export const FOLLOW_USER     = 'FOLLOW_USER';
+export const UNFOLLOW_USER   = 'UNFOLLOW_USER';
+
+// PROFILE REDUCER
 export const CREATE_PROFILE  = 'CREATE_PROFILE';
 export const UPDATE_PROFILE  = 'UPDATE_PROFILE';
 export const CREATE_AVATAR   = 'CREATE_AVATAR';
-export const REMOVE_AVATAR   = 'REMOVE_AVATAR';
+export const DELETE_AVATAR   = 'REMOVE_AVATAR';
+
+// PROMO REDUCER
 export const CREATE_PROMO    = 'CREATE_PROMO';
 export const UPDATE_PROMO    = 'UPDATE_PROMO';
 export const DELETE_PROMO    = 'DELETE_PROMO';
-export const FOLLOW_USER     = 'FOLLOW_USER';
-export const UNFOLLOW_USER   = 'UNFOLLOW_USER';
 export const LIKE_PROMO      = 'LIKE_PROMO';
 export const UNLIKE_PROMO    = 'UNLIKE_PROMO';
 export const COMMENT_PROMO   = 'COMMENT_PROMO';
