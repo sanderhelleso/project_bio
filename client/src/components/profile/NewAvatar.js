fb02009bab62ca158accbd31a03ad16ab2845f18
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-26 13:20:29 -0700

    Now renders avatar after default profile has been created

---
+++ client/src/components/profile/NewAvatar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewAvatar extends Component {
+    state = {
+        loading: false
+    }
+
+    render() {
+        return <p>Avatar creation...</p>
+    }
+}
+
+export default NewAvatar;
--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -2,19 +2,27 @@ import React, { Component } from 'react';
 
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+
 import NewProfile from '../profile/NewProfile';
+import NewAvatar from '../profile/NewAvatar';
 
 
 class Profile extends Component {
     state = {
-        new: this.props.profile.created
+        new: !this.props.profile.created
     }
 
     renderProfile() {
 
-        if (!this.state.new) {
+        if (this.state.new && this.props.profile.created) {
+            return <NewAvatar />
+        }
+
+        if (this.state.new) {
             return <NewProfile profile={this.props.profile} />
         }
+
+        return <p> You have a profile!!!</p>
     }
 
     render() {
