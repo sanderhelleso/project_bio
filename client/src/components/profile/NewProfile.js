88ffadd88dade944958a693d86e859873c9cd822
Sanderhelleso <sanderhelleso@hotmail.com>
2019-04-03 23:23:08 -0700

    Modified select focus and hover styles

--- client/src/components/profile/NewProfile.js
+++ client/src/components/profile/NewProfile.js
@@ -16,167 +16,156 @@ import { createProfile } from '../../api/profile/profile';
 import { SquareRightBorder } from '../styles/Shapes';
 
 class NewProfile extends Component {
-    state = {
-        loading: false,
-        profile: this.props.profile,
-        fields: [
-            {
-                placeholder: '@yourhandle',
-                maxLength: 30,
-                name: 'handle',
-                type: 'text',
-                onChange: e => {
-                    e.target.value = e.target.value.toLowerCase();
-                    this.handleChange(e);
-                }
-            },
-            {
-                placeholder: 'What is your name?',
-                maxLength: 70,
-                name: 'name',
-                type: 'text' 
-            },
-            {
-                placeholder: 'Tell us about yourself, what do you promote?',
-                maxLength: 150,
-                name: 'bio',
-                type: 'text' 
-            },
-            {
-                placeholder: 'Got an Instagram account? Share it!',
-                maxLength: false,
-                name: 'instagramURL',
-                type: 'text' 
-            }
-        ]
-    };
+	state = {
+		loading: false,
+		profile: this.props.profile,
+		fields: [
+			{
+				placeholder: '@yourhandle',
+				maxLength: 30,
+				name: 'handle',
+				type: 'text',
+				onChange: (e) => {
+					e.target.value = e.target.value.toLowerCase();
+					this.handleChange(e);
+				}
+			},
+			{
+				placeholder: 'What is your name?',
+				maxLength: 70,
+				name: 'name',
+				type: 'text'
+			},
+			{
+				placeholder: 'Tell us about yourself, what do you promote?',
+				maxLength: 150,
+				name: 'bio',
+				type: 'text'
+			},
+			{
+				placeholder: 'Got an Instagram account? Share it!',
+				maxLength: false,
+				name: 'instagramURL',
+				type: 'text'
+			}
+		]
+	};
 
-    saveFormState = () => {
-        this.props.setProfileAction(this.state.profile)
-    }
+	saveFormState = () => {
+		this.props.setProfileAction(this.state.profile);
+	};
 
-    componentWillUnmount() {
+	componentWillUnmount() {
+		// save form state to store on page exit
+		window.removeEventListener('beforeunload', this.saveFormState);
+	}
 
-        // save form state to store on page exit
-        window.removeEventListener('beforeunload', this.saveFormState);
-    }
+	componentDidMount() {
+		window.addEventListener('beforeunload', this.saveFormState);
+		document.querySelector('input').focus();
+	}
 
-    componentDidMount() {
-        window.addEventListener('beforeunload', this.saveFormState);
-        document.querySelector('input').focus();
-    }
+	handleChange = (e, maxLength) => {
+		if (e.target.value.length <= maxLength || !maxLength) {
+			this.setState({
+				profile: {
+					...this.state.profile,
+					[e.target.name]: e.target.value
+				}
+			});
+		}
+	};
 
+	attemptCreateProfile = async () => {
+		this.setState({ loading: true });
 
-    handleChange = (e, maxLength) => {
-        if (e.target.value.length <= maxLength || !maxLength) {
-            this.setState({ 
-                profile: {
-                    ...this.state.profile,
-                    [e.target.name]: e.target.value
-                }
-            });
-        }
-    }
+		const response = await createProfile(this.state.profile);
 
-    attemptCreateProfile = async () => {
-        this.setState({ loading: true });
+		// display notification status
+		const { toastManager } = this.props;
+		toastManager.add(response.message, {
+			appearance: response.status < 400 ? 'success' : 'error',
+			autoDismiss: response.status < 400
+		});
 
-        const response = await createProfile(this.state.profile);
+		if (response.status < 400) {
+			return this.props.setProfileAction({
+				...this.state.profile,
+				created: true
+			});
+		}
 
-        // display notification status
-        const { toastManager } = this.props;
-        toastManager.add(response.message, {
-            appearance: response.status < 400 ? 'success' : 'error',
-            autoDismiss: response.status < 400
-        });
+		this.setState({ loading: false });
+	};
 
-        if (response.status < 400) {
-            return this.props.setProfileAction({
-                ...this.state.profile,
-                created: true
-            });
-        }
+	renderFields() {
+		return this.state.fields.map((field) => {
+			return (
+				<Fragment key={field.name}>
+					<Input onChange={(e) => this.handleChange(e, field.maxLength)} {...field} />
+					{field.maxLength ? (
+						<StyledLabel>
+							{this.state.profile[field.name].length} / {field.maxLength}
+						</StyledLabel>
+					) : null}
+				</Fragment>
+			);
+		});
+	}
 
-        this.setState({ loading: false });
-    }
-
-    renderFields() {
-        return this.state.fields.map(field => {
-            return (
-                <Fragment key={field.name}>
-                    <Input 
-                        onChange={e => this.handleChange(e, field.maxLength)} 
-                        {...field}
-                    />
-                    {field.maxLength 
-                        ? <StyledLabel>
-                            {this.state.profile[field.name].length} / {field.maxLength}
-                        </StyledLabel>
-                        : null
-                    }
-                </Fragment>
-            )
-        })
-    }
-
-    render() {
-        return (
-            <StyledCont>
-                <SquareRightBorder />
-                <h1>New Profile</h1>
-                <Inputs stack={true} stretch={true}>
-                    {this.renderFields()}
-                </Inputs>
-                <Button
-                    disabled={this.state.loading}
-                    onClick={() => this.attemptCreateProfile()}
-                >
-                    <FeatherIcon icon="arrow-right" />
-                    Create profile
-                </Button>
-                <a>Need help?</a>
-            </StyledCont>
-        )
-    }
+	render() {
+		return (
+			<StyledCont>
+				<SquareRightBorder />
+				<h1>New Profile</h1>
+				<Inputs stack={true} stretch={true}>
+					{this.renderFields()}
+				</Inputs>
+				<Button disabled={this.state.loading} onClick={() => this.attemptCreateProfile()}>
+					<FeatherIcon icon="arrow-right" />
+					Create profile
+				</Button>
+				<a>Need help?</a>
+			</StyledCont>
+		);
+	}
 }
 
-const mapDispatchToProps = dispatch => {
-    return bindActionCreators({ setProfileAction }, dispatch);
-}
+const mapDispatchToProps = (dispatch) => {
+	return bindActionCreators({ setProfileAction }, dispatch);
+};
 
-
-export default connect(null, mapDispatchToProps)(NewProfile = withToastManager(NewProfile));
+export default connect(null, mapDispatchToProps)((NewProfile = withToastManager(NewProfile)));
 
 const StyledCont = styled.div`
-    max-width: 500px;
-    margin: 7.5rem auto;
-    text-align: center;
-    position: relative;
+	max-width: 500px;
+	margin: 7.5rem auto;
+	text-align: center;
+	position: relative;
 
-    h1 {
-        font-size: 3rem;
-        color: ${props => props.theme.primaryText};
-        margin-top: 0;
-    }
+	h1 {
+		font-size: 3rem;
+		color: ${(props) => props.theme.primaryText};
+		margin-top: 0;
+	}
 
+	button {
+		margin: 3rem auto 1rem auto;
+	}
 
-    button {
-        margin: 3rem auto 1rem auto;
-    }
-
-    a {
-        font-size: 0.9rem;
-        color: #9e9e9e;
-        display: block;
-        margin-top: 1rem;
-        cursor: pointer;
-    }
+	a {
+		font-size: 0.9rem;
+		color: #9e9e9e;
+		display: block;
+		margin-top: 1rem;
+		cursor: pointer;
+	}
 `;
 
 const StyledLabel = styled.span`
-    display: block;
-    float: right;
-    margin: 0.5rem 0 2rem 0;
-    font-size: 0.7rem;
-    color: #9e9e9e;
+	display: block;
+	float: right;
+	margin: 0.5rem 0 2rem 0;
+	font-size: 0.7rem;
+	color: #9e9e9e;
 `;
--- client/src/components/styles/Input.js
+++ client/src/components/styles/Input.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const inputStyles = `
+export const inputStyles = `
     outline: none;
     background-image: none;
     -webkit-box-shadow: none;
@@ -17,49 +17,47 @@ const inputStyles = `
 `;
 
 export const Inputs = styled.div`
-    
-    input {
-        display: ${props => props.stack ? 'block' : 'inline-block'};
-        min-width: ${props => props.stretch ? '100%' : 'none'};
-    }
-`
+	input {
+		display: ${(props) => (props.stack ? 'block' : 'inline-block')};
+		min-width: ${(props) => (props.stretch ? '100%' : 'none')};
+	}
+`;
 
 export const Input = styled.input`
-    ${inputStyles}
-    border: 1.5px solid ${props => props.theme.disabledBg};
+	${inputStyles} border: 1.5px solid ${(props) => props.theme.disabledBg};
 
-    &::placeholder {
-        opacity: 0.7;
-    }
+	&::placeholder {
+		opacity: 0.7;
+	}
 
-    &:focus, &:active {
-        border: 1.5px solid ${props => props.theme.primaryColor};
-        box-shadow: 0 0 0 2px ${props => props.theme.secondaryColor};
-    }
+	&:focus,
+	&:active {
+		border: 1.5px solid ${(props) => props.theme.primaryColor};
+		box-shadow: 0 0 0 2px ${(props) => props.theme.secondaryColor};
+	}
 
-    &:disabled {
-        background-color: ${props => props.theme.disabledBg};
-        cursor: not-allowed;
-        opacity: 0.7;
+	&:disabled {
+		background-color: ${(props) => props.theme.disabledBg};
+		cursor: not-allowed;
+		opacity: 0.7;
 
-        &:focus, &:active {
-            border: 1.5px solid ${props => props.theme.disabledBg};
-            box-shadow: none;
-        }
-    }
-`
+		&:focus,
+		&:active {
+			border: 1.5px solid ${(props) => props.theme.disabledBg};
+			box-shadow: none;
+		}
+	}
+`;
 
-export const Label = ({ htmlFor, text }) => (
-    <StyledLabel htmlFor={htmlFor}>{text}</StyledLabel>
-)
+export const Label = ({ htmlFor, text }) => <StyledLabel htmlFor={htmlFor}>{text}</StyledLabel>;
 
 const StyledLabel = styled.label`
-    font-weight: 600;
-    margin-bottom: 0.75rem;
-    display: block;
-    font-size: 0.7rem;
-    text-transform: uppercase;
-    letter-spacing: 2px;
-    color: #253858;
-    margin-top: 1.35rem;
+	font-weight: 600;
+	margin-bottom: 0.75rem;
+	display: block;
+	font-size: 0.7rem;
+	text-transform: uppercase;
+	letter-spacing: 2px;
+	color: #253858;
+	margin-top: 1.35rem;
 `;
