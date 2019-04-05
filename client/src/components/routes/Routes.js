e22ad8e1c6dfb2563785dd32598c4930332c4f3b
Sanderhelleso <sanderhelleso@hotmail.com>
2019-04-05 10:13:19 -0700

    Added router switches

--- client/src/components/routes/Routes.js
+++ client/src/components/routes/Routes.js
@@ -1,35 +1,25 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import DefaultRoutes from './DefaultRoutes';
 import AuthenticatedRoutes from './AuthenticatedRoutes';
-import Handle from '../handle/Handle';
 
 import { connect } from 'react-redux';
 
 class Routes extends Component {
-    
-    routeHandler() {
+	routeHandler() {
+		if (this.props.authenticated) {
+			return <AuthenticatedRoutes />;
+		} else return <DefaultRoutes />;
+	}
 
-        if (this.props.authenticated) {
-            return <AuthenticatedRoutes />
-        }
-
-        else return <DefaultRoutes />
-    }
-
-    render() {
-        return (
-            <Router>
-                {this.routeHandler()}
-                <Route exact path="/:handle" component={Handle} />
-            </Router>
-        )
-    }
+	render() {
+		return <Router>{this.routeHandler()}</Router>;
+	}
 }
 
-const mapStateToProps = state => {
-    return {  authenticated: state.user.authenticated }
-}
+const mapStateToProps = (state) => {
+	return { authenticated: state.user.authenticated };
+};
 
 export default connect(mapStateToProps, null)(Routes);
