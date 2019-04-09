4943a6bd627e040c33362ecc7b798b7ac2b2014f
Sanderhelleso <sanderhelleso@hotmail.com>
2019-04-08 18:59:36 -0700

    Added set command for redis

---
+++ server/redis/set.go
@@ -0,0 +1,18 @@
+package redis
+
+import (
+	"github.com/gomodule/redigo/redis"
+)
+
+// set excecutes the redis SET command with the 
+// given key and value passed in
+func set(c redis.Conn, key string, val interface{}) error {
+
+	_, err := c.Do("SET", key, val)
+	if err != nil {
+		return err
+	}
+
+
+	return nil
+}
