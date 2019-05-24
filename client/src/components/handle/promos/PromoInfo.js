14b087ecf1e6fabf1f24558286ffd73b36069602
Sanderhelleso <sanderhelleso@hotmail.com>
2019-05-24 04:46:02 +0200

    Update PromoInfo.js

--- client/src/components/handle/promos/PromoInfo.js
+++ client/src/components/handle/promos/PromoInfo.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const PromoInfo = ({ title }) => {
+import { getFormatedDateAndTime } from '../../../lib/format';
+
+const PromoInfo = ({ title, description, CreatedAt }) => {
 	return (
 		<StyledInfo>
 			<h5>{title}</h5>
+			<span className="postedAt">{getFormatedDateAndTime(new Date(CreatedAt).getTime())}</span>
+			<p>{description}</p>
 		</StyledInfo>
 	);
 };
@@ -14,4 +18,23 @@ export default PromoInfo;
 const StyledInfo = styled.div`
 	padding: 1rem;
 	min-height: 150px;
+
+	h5 {
+		margin: 0.25rem 0;
+		margin-bottom: 0;
+		font-size: 1.25rem;
+		letter-spacing: 2px;
+		font-weight: 800;
+	}
+
+	p {
+		font-size: 0.8rem;
+		margin-top: 1.2rem;
+	}
+
+	.postedAt {
+		font-size: 0.8rem;
+		color: #9e9e9e;
+		font-weight: 100;
+	}
 `;
