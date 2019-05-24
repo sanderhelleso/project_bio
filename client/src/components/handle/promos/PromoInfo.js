4f25841c0a785a09258789f5c0b66977f2f14e93
Sanderhelleso <sanderhelleso@hotmail.com>
2019-05-24 07:08:40 +0200

    Finished base UI for promo cards

--- client/src/components/handle/promos/PromoInfo.js
+++ client/src/components/handle/promos/PromoInfo.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import FeatherIcons from 'feather-icons-react';
 
 import { getFormatedDateAndTime } from '../../../lib/format';
 
-const PromoInfo = ({ title, description, CreatedAt }) => {
+const PromoInfo = ({ title, description, CreatedAt, discount }) => {
 	return (
 		<StyledInfo>
-			<h5>{title}</h5>
-			<span className="postedAt">{getFormatedDateAndTime(new Date(CreatedAt).getTime())}</span>
-			<p>{description}</p>
+			<div className="info-cont">
+				<h5>{title}</h5>
+				<span className="posted-at">{getFormatedDateAndTime(new Date(CreatedAt).getTime())}</span>
+				<p>{description}</p>
+			</div>
+			<StyledCont>
+				<span className="discount">
+					<span>{discount}% OFF</span>
+				</span>
+				<span className="to-promo">
+					<FeatherIcons icon="arrow-right-circle" />
+				</span>
+			</StyledCont>
 		</StyledInfo>
 	);
 };
@@ -16,8 +27,10 @@ const PromoInfo = ({ title, description, CreatedAt }) => {
 export default PromoInfo;
 
 const StyledInfo = styled.div`
-	padding: 1rem;
-	min-height: 150px;
+	.info-cont {
+		padding: 1rem;
+		min-height: 150px;
+	}
 
 	h5 {
 		margin: 0.25rem 0;
@@ -29,12 +42,51 @@ const StyledInfo = styled.div`
 
 	p {
 		font-size: 0.8rem;
-		margin-top: 1.2rem;
+		margin: 1.2rem 0;
 	}
 
-	.postedAt {
+	.posted-at {
 		font-size: 0.8rem;
 		color: #9e9e9e;
 		font-weight: 100;
+	}
+`;
+
+const StyledCont = styled.div`
+	min-height: 60px;
+	min-width: 100%;
+	position: relative;
+
+	.discount {
+		border-radius: 0% 100% 16% 84% / 100% 34% 66% 0%;
+		min-width: 135px;
+		background-color: #f0f3f5;
+		padding: 1.25rem;
+		padding-top: 1.35rem;
+		position: absolute;
+		bottom: 0;
+		left: 0;
+
+		span {
+			margin-top: 0.5rem;
+		}
+	}
+
+	span {
+		font-weight: 800;
+		color: #757575;
+		font-size: 1.25rem;
+	}
+
+	.to-promo {
+		float: right;
+		margin-right: 2rem;
+		cursor: pointer;
+		opacity: 0.6;
+
+		svg {
+			height: 2.25rem;
+			width: 2.25rem;
+		}
 	}
 `;
